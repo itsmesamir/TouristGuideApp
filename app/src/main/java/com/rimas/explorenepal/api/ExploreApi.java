b6fffd3d88045eed32ee08c23363ce3a1dba6f5d
Samir <samir.alam2992@gmail.com>
2020-04-05 11:14:46 +0545

    final commit

--- app/src/main/java/com/rimas/explorenepal/api/ExploreApi.java
+++ app/src/main/java/com/rimas/explorenepal/api/ExploreApi.java
@@ -14,11 +14,9 @@ import retrofit2.http.GET;
 public class ExploreApi {
     private static final String url= "http://10.0.2.2:80/explore/api/explore/";
     private static final String base_url = "https://jsonplaceholder.typicode.com/";
-//    private static final String Phone_url= "http://192.168.1.70:80/explore/api/explore/";
+    private static final String Phone_url= "http://192.168.1.70:80/explore/api/explore/";
 //    private static final String Phone_url= "http://192.168.30.150:80/explore/api/explore/";
-
-    
-private static final String Phone_url= "http://sameeralam.com.np/explore/api/explore/";
+//private static final String Phone_url= "http://sameeralam.com.np/explore/api/explore/";
 
 
 
